ac894e409dde2c6105f5bcd6b38f9a953adf444e
Onur Ardıç <onurardc@outlook.com>
2024-06-08 02:36:49 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    AboutusComponent,
+    ProjectsComponent,
+  ],
+
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/data.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private dataSubject = new BehaviorSubject<any>({});
+  private dataRepos = new BehaviorSubject<any>([]);
+
+  reposInfo: Observable<any>;
+  dataInfo: Observable<any>;
+
+  getData(): any {
+    this.http
+      .get('https://api.github.com/users/Onur-Ardic')
+      .subscribe((data) => {
+        this.dataSubject.next(data);
+      });
+  }
+
+  getRepos(): any {
+    this.http
+      .get('https://api.github.com/users/Onur-Ardic/repos')
+      .subscribe((repos) => {
+        this.dataRepos.next(repos);
+      });
+  }
+
+  constructor(private http: HttpClient) {
+    this.dataInfo = this.dataSubject.asObservable();
+    this.reposInfo = this.dataRepos.asObservable();
+  }
+}
